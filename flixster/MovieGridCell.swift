8675bc659d502a1e588caf6cd96984022c296620
reesecrowell <crowellreese@gmail.com>
2020-01-28 00:17:54 -0500

    Phase 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Reese Crowell on 1/27/20.
+//  Copyright © 2020 ReeseCrowell. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
